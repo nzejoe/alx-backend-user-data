30d4febb5d6235e5d09e42e3ea7b14dad1018e17
nzejoe <jonathannzete@gmail.com>
2024-07-16 07:38:07 -0700

    task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""API Routes for Authentication Service"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """ Base route for authentication service API """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
