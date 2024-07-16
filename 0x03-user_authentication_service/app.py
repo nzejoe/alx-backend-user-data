a54fa4669cb081028214f69c3ffd0e5c53c71f50
Jonathan Nzete <jonathannzete@gmail.com>
2024-07-16 07:40:41 -0700

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,3 +16,20 @@ def hello_world() -> str:
     """ Base route for authentication service API """
     msg = {"message": "Bienvenue"}
     return jsonify(msg)
+  
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user if it does not exist before"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
